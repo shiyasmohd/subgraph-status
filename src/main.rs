f277a8549317600fb0e8190d0b43684687bf2d57
shiyasmohd <shiyasmohd.official@gmail.com>
2025-03-03 16:33:04 +0530

    feat: accept multiple deployment ids

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::Parser;
 use colored::Colorize;
 use helpers::{check_for_updates, get_status_url};
 use output::display_status;
+use std::collections::BTreeSet;
 
 mod api;
 mod helpers;
@@ -12,7 +13,7 @@ mod output;
 #[command(version, about, long_about = None)]
 struct Args {
     /// Deployment ID of the subgraph (starts with Qm)
-    deployment: String,
+    deployments: Vec<String>,
 
     /// Fetch status from local graph-node
     #[clap(long, short)]
@@ -29,16 +30,29 @@ fn main() {
 
     let args = Args::parse();
 
-    let deployment_id = &args.deployment;
-    if deployment_id.starts_with("Qm") && deployment_id.len() == 46 {
+    let (valid_deployment_ids, invalid_deployment_ids): (BTreeSet<_>, BTreeSet<_>) = args
+        .deployments
+        .into_iter()
+        .partition(|id| id.starts_with("Qm") && id.len() == 46);
+
+    if !invalid_deployment_ids.is_empty() {
+        println!(
+            "Invalid deployment IDs: {}",
+            invalid_deployment_ids
+                .into_iter()
+                .collect::<Vec<_>>()
+                .join(", ")
+                .red()
+        );
+    }
+
+    for deployment_id in valid_deployment_ids {
         let url = get_status_url(&args.local);
-        match get_subgraph_status(url, deployment_id) {
+        match get_subgraph_status(url, &deployment_id) {
             Ok(res) => display_status(&res),
             Err(err) => {
                 println!("Failed to fetch status: {}", err);
             }
         }
-    } else {
-        println!("{} is not a valid deployment ID.", deployment_id.yellow());
     }
 }
